e681d8fe28095415bdb1b88c4def7988293444e2
edielam <edguard738@gmail.com>
2021-07-26 23:27:49 +0000

    unsigned int

--- print_unsigned_int.c
+++ print_unsigned_int.c
@@ -40,10 +40,57 @@ int print_u(va_list u)
  *      */
 int print_o(va_list o)
 {
-		unsigned int a[11];
-			unsigned int i, m, n, sum;
-				int count;
+unsigned int a[11];
+unsigned int i, m, n, sum;
+int count;
+n = va_arg(o, unsigned int);
+m = 1073741824; /* (8 ^ 10) */
+a[0] = n / m;
+for (i = 1; i < 11; i++)
+	{
+		m /= 8;
+		a[i] = (n / m) % 8;
+	}
+	for (i = 0, sum = 0, count = 0; i < 11; i++)
+	{
+		sum += a[i];
+		if (sum || i == 10)
+		{
+			_putchar('0' + a[i]);
+			count++;
+		}
+	}
+	return (count);
+}
 
-					n = va_arg(o, unsigned int);
-						m = 1073741824; /* (8 ^ 10) */
-							a[0] = n / m;
+/**
+ * print_b - takes an unsigned int and prints it in binary notation
+ * @b: unsigned in to print
+ *
+ * Return: number of digits printed
+ */
+int print_b(va_list b)
+{
+	unsigned int n, m, i, sum;
+	unsigned int a[32];
+	int count;
+
+	n = va_arg(b, unsigned int);
+	m = 2147483648; /* (2 ^ 31) */
+	a[0] = n / m;
+	for (i = 1; i < 32; i++)
+	{
+		m /= 2;
+		a[i] = (n / m) % 2;
+	}
+	for (i = 0, sum = 0, count = 0; i < 32; i++)
+	{
+		sum += a[i];
+		if (sum || i == 31)
+		{
+			_putchar('0' + a[i]);
+			count++;
+		}
+	}
+	return (count);
+}
