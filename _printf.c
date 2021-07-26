c1ac69642a1e978b8334560a68f5139ecc0598fe
edielam <edguard738@gmail.com>
2021-07-26 15:32:11 +0000

    retwrote the printf code

--- _printf.c
+++ _printf.c
@@ -1,37 +1,49 @@
 #include "holberton.h"
-
 /**
- * _printf - Receives the main string and all the necessary parameters to
- * print a formated string
- * @format: A string containing all the desired characters
- * Return: A total count of the characters printed
+ *_printf - prints an specified format
+ *@format: format to print
+ *Return: length of the print
  */
 int _printf(const char *format, ...)
 {
-  int printed_chars;
-  conver_t f_list[] = {
-		       {"c", print_char},
-		       {"s", print_string},
-		       {"%", print_percent},
-		       {"d", print_integer},
-		       {"i", print_integer},
-		       {"b", print_binary},
-		       {"r", print_reversed},
-		       {"R", rot13},
-		       {"u", unsigned_integer},
-		       {"o", print_octal},
-		       {"x", print_hex},
-		       {"X", print_heX},
-		       {NULL, NULL}
-  };
-  va_list arg_list;
+	function_t identity_f[] = {{'c', _printf_c}, {'s', _printf_s},
+		{'i', print_number}, {'d', print_number}, {'b', _print_b},
+		{'o', _print_o}, {'u', _print_u}, {'x', _print_x},
+		{'X', _print_X}, {'\0', NULL}};
+	va_list flist;
+	unsigned int len_printf = 0, i = 0, k = 0, flag = 0;
+	char j = '\0';
 
-  if (format == NULL)
-    return (-1);
-
-  va_start(arg_list, format);
-  /*Calling parser function*/
-  printed_chars = parser(format, f_list, arg_list);
-  va_end(arg_list);
-  return (printed_chars);
+	if (format == NULL || (format[i] == '%' && format[1] == '\0'))
+		return (-1);
+	va_start(flist, format);
+	while (format[i])
+	{
+		for (; format[i] != '%' && format[i] != '\0'; i++)
+		{
+			j = format[i];
+			len_printf += _putchar(j);
+		}
+		flag = i + 1;
+		if (format[flag] == '%' && format[i])
+			_putchar('%'), len_printf++, i += 2;
+		else if (format[flag] == '\0')
+			i++;
+		else
+		{
+			for (k = 0; identity_f[k].id && format[i]; k++)
+			{
+				if (identity_f[k].id == format[flag])
+				{
+					len_printf += identity_f[k].f(flist);
+					i += 2;
+					break;
+				}
+			}
+		}
+		if (identity_f[k].id == '\0' && format[i])
+			_putchar(format[i++]), len_printf++;
+	}
+	va_end(flist);
+	return (len_printf);
 }
