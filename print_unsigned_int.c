79568dc79ebb6fef62f76b6e66370e7735bb11d6
edielam <edguard738@gmail.com>
2021-07-27 22:40:19 +0000

    fixed issues

--- print_unsigned_int.c
+++ print_unsigned_int.c
@@ -5,31 +5,31 @@
  *   * @u: unsigned int to print
  *    *
  *     * Return: number of digits printed
- *      */
+ */
 int print_u(va_list u)
 {
-		unsigned int a[10];
-			unsigned int i, m, n, sum;
-				int count;
+	unsigned int a[10];
+	unsigned int i, m, n, sum;
+	int count;
 
-					n = va_arg(u, unsigned int);
-						m = 1000000000; /* (10 ^ 9) */
-							a[0] = n / m;
-								for (i = 1; i < 10; i++)
-										{
-													m /= 10;
-															a[i] = (n / m) % 10;
-																}
-									for (i = 0, sum = 0, count = 0; i < 10; i++)
-											{
-														sum += a[i];
-																if (sum || i == 9)
-																			{
-																							_putchar('0' + a[i]);
-																										count++;
-																												}
-																	}
-										return (count);
+	n = va_arg(u, unsigned int);
+	m = 1000000000; /* (10 ^ 9) */
+	a[0] = n / m;
+	for (i = 1; i < 10; i++)
+	{
+		m /= 10;
+		a[i] = (n / m) % 10;
+	}
+	for (i = 0, sum = 0, count = 0; i < 10; i++)
+	{
+		sum += a[i];
+		if (sum || i == 9)
+		{
+			_putchar('0' + a[i]);
+			count++;
+		}
+	}
+	return (count);
 }
 
 /**
@@ -37,16 +37,17 @@ int print_u(va_list u)
  *   * @o: unsigned int to print
  *    *
  *     * Return: number of digits printed
- *      */
+ */
 int print_o(va_list o)
 {
-unsigned int a[11];
-unsigned int i, m, n, sum;
-int count;
-n = va_arg(o, unsigned int);
-m = 1073741824; /* (8 ^ 10) */
-a[0] = n / m;
-for (i = 1; i < 11; i++)
+	unsigned int a[11];
+	unsigned int i, m, n, sum;
+	int count;
+
+	n = va_arg(o, unsigned int);
+	m = 1073741824; /* (8 ^ 10) */
+	a[0] = n / m;
+	for (i = 1; i < 11; i++)
 	{
 		m /= 8;
 		a[i] = (n / m) % 8;
